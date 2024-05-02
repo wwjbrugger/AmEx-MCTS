f0239ec1995f34f1eac639a5b8c5fedc0479e0b6
Cedric Derstroff <35600676+VanillaWhey@users.noreply.github.com>
2024-05-02 14:52:19 +0200

    Update start_mcts.py

--- src/start_mcts.py
+++ src/start_mcts.py
@@ -1,14 +1,10 @@
-import shutil
-
 import wandb
-import copy
-
 
 import numpy as np
 
 from src.utils.logging import get_log_obj
 
-from src.environments.gym_game import GymGame, CompilerGymWrapper
+from src.environments.gym_game import GymGame, make_env
 from src.amex_mcts import AmEx_MCTS
 from src.classic_mcts import ClassicMCTS
 from src.config_mcts import parse_args
@@ -16,8 +12,6 @@ from src.utils.get_grammar import read_grammar_file
 from src.environments.find_equation_game import FindEquationGame
 import random
 import time
-import compiler_gym
-import os
 
 
 def run_gym(args) -> (float, float):
@@ -30,19 +24,7 @@ def run_gym(args) -> (float, float):
     random.seed(args.seed)
     logger = get_log_obj(args=args, name=args.mcts_engine)
 
-    # create the one and only environment
-    CompilerGymWrapper.env = compiler_gym.make(
-            "llvm-v0",  # selects the compiler to use
-            benchmark=args.env_str,  # selects the program to compile
-            observation_space="Autophase",  # selects the observation space
-            reward_space="IrInstructionCountOz",
-            # selects the optimization target
-        )
-
-    env = CompilerGymWrapper(
-        max_episode_steps=args.max_episode_steps,
-        args=args
-    )
+    env = make_env(args.env_str, args.max_episode_steps)
     game = GymGame(args, env)
     wandb.config.update(game.env.spec.kwargs)
 
@@ -59,6 +41,7 @@ def run_gym(args) -> (float, float):
     discounted_return = 0
     gamma = 1.0
     start_time = time.time()
+    next_state = None
 
     while not state.done:
         pi, v = mcts_engine.run_mcts(state=state,
@@ -79,15 +62,12 @@ def run_gym(args) -> (float, float):
         print(a)
     end_time = time.time()
     wandb.log({
-        "max_list_actions": str(state.env.max_list.max_list_state[-1]),
-        "max_list_reward": state.env.max_list.max_list_keys[-1][0],
         "mcts_actions: ": next_state.hash,
         "run_time": end_time - start_time
     })
 
     # Cleanup environment and GameHistory
-    # close only existing env
-    CompilerGymWrapper.env.close()
+    env.close()
 
     # Log results
     logger.debug(f"Return: {undiscounted_return}")
@@ -139,7 +119,10 @@ def run_equation(args) -> (float, float):
         undiscounted_return += r
         gamma *= args.gamma
 
-        logger.debug(f"{i}, {a}, {pi}, {v}, {next_state.observation['current_tree_representation_str']}")
+        logger.debug(
+            f"{i}, {a}, {pi}, {v},"
+            f"{next_state.observation['current_tree_representation_str']}"
+        )
 
         # Log results
         logger.debug(f"Return: {undiscounted_return}")
